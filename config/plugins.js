c9aab3a92c98a6d1877c9e205bbecf2d9326e542
Thomas <thomas.lin@maxwin.com.tw>
2024-06-18 20:18:07 +0800

    feat(app-work): 增加api

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,10 @@
-module.exports = () => ({});
+module.exports = () => ({
+  'transformer': {
+    enabled: true,
+    config: {
+      responseTransforms: {
+        removeAttributesKey: true,
+      },
+    }
+  },
+});
